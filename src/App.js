9d3ffbd1419b91432e4033d7e809c12cc77510ab
sallahTech <donlyrlmrktr@gmail.com>
2022-12-17 21:06:38 +0200

    removed authentication for now

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react'
-import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import React from "react";
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <AuthWrapper>
       <Router>
         <Switch>
-          <PrivateRoute path='/' exact>
+          <PrivateRoute path="/" exact>
             <Dashboard></Dashboard>
           </PrivateRoute>
-          <Route path='/login'>
-            <Login />
+          <Route path="/login">
+            <Dashboard></Dashboard>
+            {/* <Login /> */}
           </Route>
-          <Route path='*'>
+          <Route path="*">
             <Error />
           </Route>
         </Switch>
       </Router>
     </AuthWrapper>
-  )
+  );
 }
 
-export default App
+export default App;
